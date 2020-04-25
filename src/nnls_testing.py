7bd83b852e14bdc931d243f734320e0e65f9e2fd
jvendrow <jvendrow@gmail.com>
2020-04-25 10:25:04 -0700

    Improvements to nnls_testing

--- src/nnls_testing.py
+++ src/nnls_testing.py
@@ -9,37 +9,105 @@ from scipy.optimize import nnls
 from time import time
 
 
-nnls_times = []
-dimensions = np.arange(10, 400, 20)
+class nnls_testing():
+
+    def __init__(self):
+
+        return
+
+    def test(self, repetitions, dimensions, optimizers, names, verbose=True):
+        """
+        Measures the speed of each optimizer at each dimensions,
+        averaged over repetitions
+
+        Parameters
+        ----------
+        repetitions: int
+            The number of times to run at optimizer at each dimensions
+            for precision. 
+
+        dimensions: numpy array
+            A list of dimensions of matrix A and vector x to use 
+            for testing.
+
+        optimizers: list
+            A list of nonnegative least squares functions that
+            we will test the complexity of.
+
+        names: list
+            A list corresponding to the optimizers list of what
+            to call the optimizers for plotting
+
+        """
+
+        nnls_times = []
+
+        for d in dimensions:
+
+            #define matrix A and vector x
+            #------------------------------
+            #A = np.abs(np.random.rand(d, d))
+
+            #x = np.abs(np.random.rand(d))
 
 
-repetitions = 150
+            #Test the speed of scipy.optimize.nnls
+            #-----------------------------------
 
-for d in dimensions:
-    print(d)
+            for index, optimizer in enumerate(optimizers):
 
-    #define matrix A and vector x
-    #------------------------------
-    A = np.abs(np.random.rand(d, d))
+                start = time()
 
-    x = np.abs(np.random.rand(d))
+                for i in range(repetitions):
+
+                    A = np.abs(np.random.rand(d, d))
+
+                    x = np.abs(np.random.rand(d))
+
+                    [s, res] = nnls(A, x)
+
+                end = time()
+
+                #Print and store results
+                #-----------------------
+                print(d)
+                print(names[index] + ": " + str(end-start))
+
+                if d == dimensions[0]:
+                    nnls_times.append([end-start])
+
+                else:
+                    nnls_times[index].append(end-start)
+
+            self.repetitions = repetitions
+            self.dimensions = dimensions
+            self.names = names
+            self.nnls_times = nnls_times
+
+    def plot(self):
+        """
+        Plots the time complexities of each optimizer
+
+        """
+        #Plot the times for nnls
+        #-----------------------
+        for nnls_time in self.nnls_times:
+            plt.plot(self.dimensions, nnls_time)
+
+        plt.xlabel("dimension")
+        plt.ylabel("time (s) for " +  str(self.repetitions) + " runs")
+        plt.legend(self.names)
+
+        plt.show()
 
 
-    #Test the speed of scipy.optimize.nnls
-    #-----------------------------------
-    start = time()
+testing = nnls_testing()
 
-    for i in range(repetitions):
+repetitions = 25
+dimensions = np.arange(20, 400, 20)
+optimizers = [nnls, nnls]
+names = ["scipy.optimize.nnls1", "scipy.optimize.nnls2"]
 
-        [s, res] = nnls(A, x)
+testing.test(repetitions, dimensions, optimizers, names, verbose=True)
 
-    end = time()
-
-    #Print and store results
-    #-----------------------
-    print(end - start)
-    nnls_times.append(end-start)
-
-
-plt.plot(dimensions, nnls_times)
-plt.show()
+testing.plot()
